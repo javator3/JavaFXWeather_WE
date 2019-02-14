f4e988baf48326aa94398ab4937dd3323dc6d501
Eliza <zielonykuferek@gmail.com>
2019-02-14 12:23:08 +0100

    Dodanie repo

---
+++ src/main/java/pl/sda/openweather/controller/RootController.java
@@ -0,0 +1,69 @@
+package pl.sda.openweather.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import pl.sda.openweather.model.Weather;
+import pl.sda.openweather.model.WeatherService;
+
+
+public class RootController implements Initializable {
+
+    private String finalURL ="http://api.apixu.com/v1/current.json?key=8e42af245b4240d7bc192407191002&q=";
+
+
+    @FXML
+    private Button search;
+
+    @FXML
+    private TextField city;
+
+    @FXML
+    private Label temp;
+
+    @FXML
+    private Label feelTemp;
+
+    @FXML
+    private Label realResult;
+
+    @FXML
+    private Label feelResult;
+
+    public void initialize(URL location, ResourceBundle resources) {
+        city.setText("nazwa miasta ");
+
+    }
+
+    public void setCity(ActionEvent actionEvent) {
+
+
+        URL jsonURL = null;
+        try {
+            jsonURL = new URL(finalURL+ city.getText());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        Weather weather = null;
+        try {
+            weather = objectMapper.readValue(jsonURL, Weather.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        realResult.setText(String.valueOf(weather.getCurrent().getTemp_c()));
+        feelResult.setText(String.valueOf(weather.getCurrent().getFeelslike_c()));
+
+
+
+    }
+}
